edd0abdfc5a37661045f6688fe46d7d46dd5a391
Zhengzi <flymaple1213@gmail.com>
2016-02-23 17:52:27 +0800

    populate the basic block from IDA into pickle file

---
+++ entity_class_def.py
@@ -0,0 +1,22 @@
+class BasicBlock:
+	def __init__(self):
+		self.start_address = None
+		self.end_address = None
+		self.instr = None
+		self.func = None
+		#self.program = None
+		
+	def write_to_file(self):
+		return
+	def load_from_file(self):
+		return
+		
+
+class Function:
+	def __init__(self, start_address):
+		self.start_address = start_address
+		self.end_address = None
+		self.bb_list = []
+		self.in_funcs = None
+		self.out_funcs = None
+		self.program = None
--- tanslate.py
+++ tanslate.py
@@ -2,8 +2,22 @@
 #import archinfo
 from idautils import *
 from idaapi import *
+from entity_class_def import BasicBlock as bb_class
+import pickle
 
+
+def creat_bb_instance(bb, func_name):
+	bb_ins = bb_class(5)
+	bb_ins.start_address = bb.startEA
+	bb_ins.end_address = bb.endEA		
+	bb_ins.instr = tanslate(bb)
+	bb_ins.func = func_name
+	pickle.dump(bb_ins, file("C:\\Users\\user1\\Desktop\\test\\" + str(hex(bb.startEA))+".pickle",'w'))
+	#bb.program = None
+	
+	
 def tanslate(bb):
+	instruction = []
 	for head in Heads(bb.startEA,bb.endEA):
 		if isCode(getFlags(head)):
 			#I give up now, so I use a very silly way to do this.
@@ -18,7 +32,9 @@ def tanslate(bb):
 			#need linux enviroment to install pyvex pacakage			
 			#irsb = pyvex.IRSB(instr, 0x400000, archinfo.ArchX86())
 			print instr
+			instruction.append(instr)
 			
+	return 	instruction	
 	#irsb = pyvex.IRSB(bb, archinfo.VexArchX86)
 
 #format the instr convert "0x4" to "\x04"	
@@ -30,10 +46,14 @@ def format_instr(byte):
 	return s
 	
 def main():
-	func_ea = here()
-	print GetFunctionName(func_ea)
-	for bb in FlowChart(get_func(func_ea)):
-		tanslate(bb)
+	for seg in Segments():		
+		if SegName(seg) == ".text":
+			functions = Functions(seg)		
+			for func_ea in functions:		
+				print GetFunctionName(func_ea)
+				for bb in FlowChart(get_func(func_ea)):
+				#tanslate(bb)
+					creat_bb_instance(bb, GetFunctionName(func_ea))
 	return
 
 
