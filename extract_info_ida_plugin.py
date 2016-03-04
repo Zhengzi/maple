cbbb056c45ae0bac350b45938e5eb4870b519a1f
Zhengzi <flymaple1213@gmail.com>
2016-03-04 15:05:07 +0800

    add find_the_number_of_indirect_callv

--- extract_info_ida_plugin.py
+++ extract_info_ida_plugin.py
@@ -1,6 +1,7 @@
 from idautils import *
 from idaapi import *
 import networkx as nx
+import time
 
 #iteratively loop over every basic block to get cfg within one function, one basic block will be a node in the graph
 def extract_intra_function_cfg():
@@ -112,15 +113,22 @@ def extract_inter_function_cfg():
 			
 			#for ref in CodeRefsFrom(func_ea, 1):
 			#	print "  calling %s(0x%x)" % (GetFunctionName(ref), ref)
-			
+	'''	
 	#testing
-	#print cfg.number_of_nodes()
-	#print cfg.number_of_edges()
-	#nodes = cfg.nodes()
-	#for node in nodes:
-	#	print node
+	print cfg.number_of_nodes()
+	print cfg.number_of_edges()
+	#print cfg.node()
+	nodes = cfg.nodes()
+	print
+	for node in nodes:
+		print "parent: "
+		print hex(node)
+		print "child: "
+		ns = cfg.successors(node)
+		for n in ns:
+			print hex(n)
 	#endtesting
-	
+	'''
 	return cfg
 
 def count_in_out_edge(cfg):	
@@ -135,7 +143,51 @@ def count_in_out_edge(cfg):
 		if in_degree + out_degree > 0:
 			ratio = (float)(out_degree)/(float)(in_degree + out_degree)
 		#manaully set the longest function name to 50 char, largest number of func call to 9999
-		print 'function: {0:50} out_degree: {1:4d} in_degree: {2:4d} ratio: {3:6f}'.format(func_name, out_degree, in_degree, ratio)
+		#print 'function: {0:50} out_degree: {1:4d} in_degree: {2:4d} ratio: {3:6f}'.format(func_name, out_degree, in_degree, ratio)
+	return
+	
+#this func aims to find the number of indirect calls in the function, as well as find any particular calls in the function.
+#it returns the count of the calls 	
+def find_the_number_of_indirect_call():
+	count = 0
+	count_memcpy = 0
+	for seg in Segments():	
+		
+		if SegName(seg) == ".text":
+	
+			functions = Functions(seg)		
+			for func_ea in functions:	
+				info = "" + GetFunctionName(func_ea) + "\n"
+				#print GetFunctionName(func_ea)
+				flag1 = False
+				flag2 = False
+				indiret_count = 0
+				total_count = 0
+				for bb in FlowChart(get_func(func_ea)):
+					for head in Heads(bb.startEA,bb.endEA):
+						if isCode(getFlags(head)):
+							mnem = GetMnem(head)
+							if mnem == "call":
+								op = GetOpnd(head, 0)
+								total_count = total_count + 1
+								#if "[" in op:
+								if "eax" in op or "ebx" in op or "ecx" in op or "edx" in op or "esi" in op or "edi" in op or "ebp" in op or "esp" in op:
+								#if op == "eax" or op == "ebx" or op == "ecx" or op ==  "edx" or op == "esi" or op == "edi" or op == "esp" or op == "ebp":
+									indiret_count = indiret_count + 1
+									count += 1
+								if "memcpy" in op:
+									flag1 = True
+								if "malloc" in op:								
+									flag2 = True
+				ratio = 0 if total_count==0 else (float)(indiret_count)/(float)(total_count)
+				info += "indrect call: " + str(indiret_count) + " indrect call ration: " + str(ratio)
+				#print info	
+				if flag1 and flag2:
+					count_memcpy += 1
+					flag1 = False
+					flag2 = False
+
+	print count_memcpy
 	return
 	
 def count_in_out_edge_without_api(cfg, api_list):	
@@ -164,9 +216,10 @@ def count_in_out_edge_without_api(cfg, api_list):
 		if in_degree + out_degree > 0:
 			ratio = (float)(out_degree)/(float)(in_degree + out_degree)
 		#manaully set the longest function name to 50 char, largest number of func call to 9999
-		print 'function: {0:50} out_degree: {1:4d} in_degree: {2:4d} ratio: {3:6f}'.format(func_name, out_degree, in_degree, ratio)
+		#print 'function: {0:50} out_degree: {1:4d} in_degree: {2:4d} ratio: {3:6f}'.format(func_name, out_degree, in_degree, ratio)
 	return
-	
+
+#generate the function list from the given binary and store it in to a files
 def _get_function_list():
 	func_list = []
 	for seg in Segments():	
@@ -182,7 +235,8 @@ def _get_function_list():
 		f.write("\n")
 	f.close
 	return func_list
-	
+
+#load the fucntion list from the hhd, I have not uploaded the function list yet.	
 def load_func_list():
 	func_list = []
 	f = open('C:\\Users\\Xu Zhengzi\\Desktop\\func_list.txt','r')
@@ -207,13 +261,18 @@ def main():
 	#wait until IDA finishing loading the project
 	idaapi.autoWait()
 	
-	extract_intra_function_cfg()
-	func_cfg = extract_inter_function_cfg()
+	#extract_intra_function_cfg()
+	#func_cfg = extract_inter_function_cfg()
 	#count_in_out_edge(func_cfg)
 	#_get_function_list()
-	func_list = load_func_list()
-	count_in_out_edge_without_api(func_cfg, func_list)
+	#func_list = load_func_list()
+	t0 = time.time()
+	#count_in_out_edge_without_api(func_cfg, func_list)
+	print time.time() - t0
+	find_the_number_of_indirect_call()
 	print "Finish"
 		
 if __name__ == '__main__':
-    main()
+	t0 = time.time()
+	main()
+	print time.time() - t0
