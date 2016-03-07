d1cf5fca41e3f8a4ded2615c3cdcf7a2859b37f4
Zhengzi <flymaple1213@gmail.com>
2016-03-07 10:35:53 +0800

    add some experiment functions

--- analysis_basic_block.py
+++ analysis_basic_block.py
@@ -92,6 +92,118 @@ def get_bb_basic_info(bb):
 	#TODO: use a info class to store the info getted, or to store it in the bb wrapper class
 	return info
 	
+#this is the ndss paper's feature of a function
+#this is for experiment purpose, will not use it in practice
+'''
+data_transfer = ['movsx','movss', 'movzx','cmov','cmova','cmovae','cmovb','cmovbe','cmovc','cmove','cmovg','cmovge','cmovl','cmovle','cmovna','cmovnae','cmovnb','cmovnbe','cmovnc','cmovne','cmovng','cmovnge','cmovnl','cmovnle','cmovno','cmovnp','cmovns','cmovnz','cmovo','cmovp','cmovpe','cmovpo','cmovs','cmovz','set','seta','setae','setb','setbe','setc','sete','setg','setge','setl','setle','setna','setnae','setnb','setnbe','setnc','setne','setng','setnge','setnl','setnle','setno','setnp','setns','setnz','seto','setp','setpe','setpo','sets','setz','xchg','cmpxchg']
+arithmetic = ['add','adc','sub','sbb','mul','imul','div','neg', 'idiv', 'inc', 'dec']
+stack = ['pusha', 'pushf', 'popa', 'popf']
+logical = ['and','or','not']
+shift_rotate = ['shr','shl','sal','sar','rol','ror','rcl','rcr']
+control_transfer = ['jno','jnp','jns','jo','jp','jpe','jpo','js','jecxz','jcxz']
+loop = ['loop','loopne','loopnz','loope','loopz']
+string = ['cmps','cmpsb','cmpsw','cmpsd','cmpsq','lods','lodsb','lodsw','lodsd','lodsq','movs','movsb','movsw','movsd','movsq','scas','scasb','scasw','scasd','scasq','stos','stosb','stosw','stosd','stosq','rep']
+flag = ['cld','clc','stc','std', 'cmc', 'sti', 'cli']
+sign = ['cbw', 'cwd', 'cwde', 'cdq', 'csq']
+misc =['hlt', 'retn', 'retf']
+fpu = ['fld', 'fstp']
+port = ['in', 'out']
+common = ['mov', 'jmp', 'cmp', 'lea', 'jz', 'jnz', 'jmp', 'jl', 'jle', 'jb', 'jbe']
+
+call_tags = ['mem', 'str', 'cpy', 'cat', 'move', 'host', 'reg', 'set', 'get', 'open', 'write', 'read', 'close', 'error', 'len', 'alloc', 'brk', 'fork', 'io', 'heap', 'name', 'crypt', 'net', 'socket', 'sys', 'file', 'load', 'lib', 'cache', 'mutex', 'proc', 'thread', 'process', 'sleep', 'chk', 'free', 'put', 'print']
+'''
+
+
+def Ndss_imp():
+	t0 = time.time()
+	t_count = 0
+	for seg in Segments():	
+	
+		if SegName(seg) == ".text" or SegName(seg) == ".plt":
+	
+			#functions = Functions(seg)
+			#for func_ea in functions:				
+				bb_count = 0
+				instr_count = 0
+				func_ea = here()
+				
+				print FuncItems(func_ea)
+				#getStack(func_ea)
+				var_count = getStack(func_ea)
+				#print len(list(CodeRefsTo(func_ea, 1)))
+				#print hex(func_ea)
+				redirection_count = 0
+				incoming_call_count = len(list(CodeRefsTo(func_ea, 1)))
+				outcoming_call_count = 0
+				transfer_instr_count = 0
+				logic_instr_count = 0
+				logical = ['and','or','not','xor']
+				misc =['hlt', 'retn', 'retf']
+				edges_count = 0
+				for bb in FlowChart(get_func(func_ea)):
+					bb_count += 1
+					t_count += 1
+					#print hex(bb.startEA)
+					for head in Heads(bb.startEA,bb.endEA):
+						if isCode(getFlags(head)):
+							instr_count += 1
+							mnem = GetMnem(head)
+							if mnem == "call":
+								outcoming_call_count  += 1
+								redirection_count += 1
+							elif "j" in mnem:
+								transfer_instr_count += 1
+								edges_count += 2
+								redirection_count += 1
+							if mnem == "jmp":
+								edges_count -= 1
+							if mnem in logical:
+								logic_instr_count += 1
+							if mnem in misc:
+								redirection_count += 1
+								
+								
+	print t_count
+	print time.time() - t0	
+	
+	print "bb_count: " + str(bb_count)
+	print "incoming_call_count: " + str(incoming_call_count)
+	print "instr_count: " + str(instr_count)
+	print "outcoming_call_count: " + str(outcoming_call_count)
+	print "transfer_instr_count: " + str(transfer_instr_count)
+	print "edges_count: " + str(edges_count)
+	print "logic_instr_count: " + str(logic_instr_count)
+	print "var_count: " + str(var_count)
+	print "redirection_count: " + str(redirection_count)
+
+	
+def getStack(address):
+	'gets variables used on the stack'
+	# Related reads and/or pointers on stacks in IDA
+	# http://zairon.wordpress.com/2008/02/15/idc-script-and-stack-frame-variables-length/
+	# https://github.com/moloch--/IDA-Python/blob/master/Stack_Calculator.py
+	stackFrame = GetFrame(address)
+	lastEntry = GetLastMember(stackFrame)
+	count = 0
+	c = 0
+	#stack = []
+	while count <= lastEntry:
+			localName = GetMemberName(stackFrame,count)
+			size = GetMemberSize(stackFrame, count)
+			flag = GetMemberFlag(stackFrame, count)
+			if localName == None or size == None or flag == -1:
+					count += 1
+					continue 
+			#stack.append((localName, size, hex(flag)))
+			count += size
+			c += 1
+	# returns stack variable list[tuple(localName, size, hex(flags))]
+	return c	
+	
+def t():
+	func_ea = here()
+	print len(list(CodeRefsTo(func_ea, 1)))
+	
 def main():
 	func_ea = here()
 	print GetFunctionName(func_ea)
@@ -100,4 +212,7 @@ def main():
 		print info
 	
 if __name__ == '__main__':
-    main()
+    #main()
+	Ndss_imp()
+
+	#t()
